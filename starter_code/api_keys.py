2eaff91ca2892a13f1192b6dbd73f453d6251c42
Renee Alexis Perez <163601278+reneealexiis@users.noreply.github.com>
2024-05-09 15:03:49 -0400

    Final Work

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "d480ed069282c2ea3f7b2cb240045ab2"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "173e5b268f3c479c9c082655eaa0e90e"
